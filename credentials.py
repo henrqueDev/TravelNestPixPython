c093961e68f61bf1e098a65f0db81b603c9153ec
henrqueDev <lhdeveloper137@gmail.com>
2023-10-16 21:57:52 -0300

    Revert "organizar projeto"
    This reverts commit f62bddff52ee19e744dbc5f2833d808bf4965da5.

--- credentials.py
+++ credentials.py
@@ -1,9 +1,6 @@
-# encoding: utf-8
-
-# Insira aqui suas credenciais
 CREDENTIALS = {
-    'client_id': 'Client_Id_04142944c600f7136a2284042e5030117f3a58c7',
-    'client_secret': 'Client_Secret_25ed88353071136a644362de77b5bba522cc0c27',
+    'client_id': '',
+    'client_secret': '',
     'sandbox': False, # True: Ambiente de Homologação |  False: Ambiente de Produção
     'certificate': 'C:/Users/henrique/OneDrive/Documentos/TravelNestPixPython/producao-307980-Teste.pem'
 }
