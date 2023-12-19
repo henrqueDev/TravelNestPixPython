9a5cff2e2e2112103ea1a091d8a2872cf530c349
henrqueDev <lhdeveloper137@gmail.com>
2023-12-19 08:04:23 -0300

    versao 4 final

--- service/codeGeneration/PixCodeGenerator.py
+++ service/codeGeneration/PixCodeGenerator.py
@@ -10,6 +10,8 @@ from efipay import EfiPay
 import credentials
 from repository.log_pix_charge_repository import LogPixChargeRepository
 
+from dotenv import load_dotenv
+load_dotenv()
 
 class PixCodeGenerator():
     
@@ -17,31 +19,37 @@ class PixCodeGenerator():
         self.efi = EfiPay(credentials.CREDENTIALS)
         self.log_repository = LogPixChargeRepository()
 
-    def generate_pix_code(self, pix_key, user_id, total_price, hotel_id, address_requester, check_in, check_out, room_option_id, children_quantity, adults_quantity):
-        body = {
-            'calendario': {
-                'expiracao': 3600
-            },
-            'valor': {
-                'original': "{:.2f}".format(float(total_price))
-            },
-            'chave': pix_key,
-            'solicitacaoPagador': 'Cobrança dos serviços prestados.'
-        }
-        response =  self.efi.pix_create_immediate_charge(body=body)
-        
-        
-        print(response)
+    def generate_pix_code(self, pix_key, user_id, total_price, hotel_id, address_requester, check_in, check_out, room_option_id, children_quantity, adults_quantity, user_email, hotel_name):
+        try:
+            body = {
+                'calendario': {
+                    'expiracao': 3600
+                },
+                'valor': {
+                    'original': "{:.2f}".format(float(total_price))
+                },
+                'chave': pix_key,
+                'solicitacaoPagador': 'Cobrança dos serviços prestados.'
+            }
+            response =  self.efi.pix_create_immediate_charge(body=body)
+            
+            
+            print(response)
 
-        params = {
-            'id': response['loc']['id']
-        }
-        response_code =  self.efi.pix_generate_qrcode(params=params) 
+            params = {
+                'id': response['loc']['id']
+            }
+            response_code =  self.efi.pix_generate_qrcode(params=params) 
+            
+            r = requests.post(os.getenv('endpoint_redis'), json={"pix_key": pix_key, "user_id": user_id, "hotel_id": hotel_id, "cob_id": response['txid'], 
+                                                                            "total_price":  "{:.2f}".format(float(total_price)), "check_in": check_in, "check_out": check_out, "room_option_id": room_option_id, 
+                                                                            "status": response["status"], "children_quantity": children_quantity, "adults_quantity": adults_quantity, "user_email": user_email, 
+                                                                            "hotel_name": hotel_name})
+            if r.status_code == 201:
+                self.log_repository.save(pix_key, address_requester, user_id, total_price, hotel_id, response['txid'],  check_in, check_out, room_option_id)
+                return response_code['qrcode']
+        except Exception as e:
+            return "O serviço de cobrança está enfrentando problemas internos. Tente Novamente mais tarde"
         
-        requests.post("http://127.0.0.1:5000/consultaPagamentos", json={"pix_key": pix_key, "user_id": user_id, "hotel_id": hotel_id, "cob_id": response['txid'], 
-                                                                        "total_price":  "{:.2f}".format(float(total_price)), "check_in": check_in, "check_out": check_out, "room_option_id": room_option_id, "status": response["status"],
-                                                                          "children_quantity": children_quantity, "adults_quantity": adults_quantity  })
-        self.log_repository.save(pix_key, address_requester, user_id, total_price, hotel_id, response['txid'],  check_in, check_out, room_option_id)
-        return response_code['qrcode']
 
  
